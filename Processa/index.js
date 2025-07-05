05fe3061feadb3c3d76c2f11746815ccd9486d46
rafael45 <rafael450140@gmail.com>
2025-07-05 01:15:27 -0300

    Update index.js

--- Processa/index.js
+++ Processa/index.js
@@ -1,12 +1,11 @@
 module.exports = async function (context, req) {
-    const nome = req.query.nome || "amigo";
     context.res = {
         status: 200,
         headers: {
             "Content-Type": "application/json"
         },
         body: {
-            message: `Olá, ${nome}, Deus te abençoe.`
+            message: "Olá, tudo funcionando corretamente."
         }
     };
 };
